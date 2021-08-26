50f6b1695afc7f08d92c9b6cfaa37bd6013d03f5
subha7598 <38026269+subha7598@users.noreply.github.com>
2021-08-26 18:42:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import  ReactDom from 'react-dom';
+import App from './App';
+//import Login from './Login';
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//import Otp from './Otp';
+//import Forms from './Forms';
+// import Redux from './Redux';
+
+
+ReactDom.render(<App/>,document.getElementById('root'));
+
+
+
+
+
+
+
+
+
